ade882564b913e8a4cb52966570d01a6f6b5274e
norbertown02 <wiederkehrneto@hotmail.com>
2026-07-03 10:51:52 -0300

    Refina identidade visual do Nutrialle Gestão

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 import { NavLink } from 'react-router-dom'
-import logo from '../assets/logo-nutrialle.jpg'
+import logo from '../assets/logo-nutrialle.png'
 import {
   IconLayoutDashboard,
   IconReceipt,
