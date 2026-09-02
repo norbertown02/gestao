58e7021fe9597e9d9ab4dbb153c21cfb72d087f6
norbertown02 <wiederkehrneto@hotmail.com>
2026-09-02 09:28:32 -0300

    Ativar Dashboard com filtros de produto

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { AuthProvider, useAuth } from './lib/useAuth'
 import Sidebar from './components/Sidebar'
 import SplashScreen from './components/SplashScreen'
 import Login from './screens/Login'
-import Dashboard from './screens/Dashboard'
+import DashboardFiltrado from './screens/DashboardFiltrado'
 import VendasFiltradasV2 from './screens/VendasFiltradasV2'
 import Visitas from './screens/Visitas'
 import Checklists from './screens/Checklists'
@@ -27,32 +27,32 @@ import AuditoriaFinanceira from './screens/AuditoriaFinanceira'
 function AppContent() {
   const { user, loading, showSplash } = useAuth()
   if (loading || showSplash) return <SplashScreen />
-  if (!user)   return <Login />
+  if (!user) return <Login />
   return (
     <div className="layout">
       <Sidebar />
       <div className="main">
         <Routes>
-          <Route path="/"           element={<Dashboard />} />
-          <Route path="/vendas"     element={<VendasFiltradasV2 />} />
-          <Route path="/visitas"    element={<Visitas />} />
+          <Route path="/" element={<DashboardFiltrado />} />
+          <Route path="/vendas" element={<VendasFiltradasV2 />} />
+          <Route path="/visitas" element={<Visitas />} />
           <Route path="/checklists" element={<Checklists />} />
-          <Route path="/carteira"   element={<Carteira />} />
+          <Route path="/carteira" element={<Carteira />} />
           <Route path="/vendedores" element={<Vendedores />} />
-          <Route path="/produtos"   element={<Produtos />} />
-          <Route path="/estoque"    element={<Estoque />} />
+          <Route path="/produtos" element={<Produtos />} />
+          <Route path="/estoque" element={<Estoque />} />
           <Route path="/financeiro" element={<Financeiro />} />
-          <Route path="/dre"        element={<DRE />} />
+          <Route path="/dre" element={<DRE />} />
           <Route path="/financeiro/importar" element={<ImportarFechamento />} />
           <Route path="/financeiro/auditoria" element={<AuditoriaFinanceira />} />
-          <Route path="/regioes"    element={<Regioes />} />
+          <Route path="/regioes" element={<Regioes />} />
           <Route path="/pipeline" element={<Pipeline />} />
           <Route path="/time" element={<DashboardTime />} />
           <Route path="/metas" element={<Metas />} />
           <Route path="/relatorio" element={<RelatorioMensalFiltradoMulti />} />
           <Route path="/fechamentos" element={<Fechamentos />} />
           <Route path="/cotacoes" element={<Cotacoes />} />
-          <Route path="/config"     element={<Config />} />
+          <Route path="/config" element={<Config />} />
         </Routes>
       </div>
     </div>
