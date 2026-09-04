076c2c33da21036e631ee611c70627fd61131707
norbertown02 <wiederkehrneto@hotmail.com>
2026-09-04 17:48:12 -0300

    style: ativar novo cabecalho mobile Nutrialle

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import './commerce-filters.css'
 import './mobile.css'
 import './mobile-ux.css'
+import './mobile-brand.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
