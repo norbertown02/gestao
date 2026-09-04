1005d417d88cac4188e4913d0239e3275143ff11
norbertown02 <wiederkehrneto@hotmail.com>
2026-09-04 15:34:00 -0300

    style: ativar UX mobile refinada

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './commerce-filters.css'
 import './mobile.css'
+import './mobile-ux.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
